5deae55d04be4adab476c7aa88e74f8be1b1cbb8
Milen Dyankov <milendyankov@gmail.com>
2025-12-20 18:37:41 +0100

    redirect on trailing slash

---
+++ nuxt-site/app/middleware/trailing-slash.global.ts
@@ -0,0 +1,6 @@
+export default defineNuxtRouteMiddleware((to) => {
+  if (to.path !== '/' && to.path.endsWith('/')) {
+    const newPath = to.path.slice(0, -1) || '/'
+    return navigateTo(newPath, { redirectCode: 301 })
+  }
+})
