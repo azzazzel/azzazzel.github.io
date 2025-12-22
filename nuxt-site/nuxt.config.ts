c26c20e5f81dbda3a0841a0f8a1fedb8fe0f00e0
Milen Dyankov <milendyankov@gmail.com>
2025-12-22 19:56:40 +0100

    Update font

--- nuxt-site/nuxt.config.ts
+++ nuxt-site/nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
     '@nuxt/scripts',
     'nuxt-llms',
     'nuxt-feedme',
+    '@nuxt/fonts',
   ],
   css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
