0a1a15d210abc978e008c61b5f7f800c76c755ba
Milen Dyankov <milendyankov@gmail.com>
2026-01-23 17:13:44 +0100

    Fix subscribe form

--- app/components/SubscribeForm.vue
+++ app/components/SubscribeForm.vue
@@ -3,14 +3,23 @@
     :schema="schema"
     :state="state"
     class="space-y-4"
-    method="post"
+    method="POST"
     action="https://api.follow.it/subscription-form/NTByT1ZKVWNRZWJycTVwWnNCdGVYTG1ieWhlc1VRWWNpZHFXVVJkc3V5cFVUZ1hZNXFQTFZiL3ZheWc2S2FGSDlTSnFWNndTZGlxaUlaVjNTWlltTzNlOUF6YmtvcXBMb2oxalRUUnRzZHdlNTVOekdiYnc5bG5OeXFrU3k4U0V8bm9qcmNYQVVUK2NMbEN0cjRuNHg5dlByeDhmWk5rVldWeFk2NExmNjVrST0=/8"
+    @submit="onSubmit"
   >
     <UFormField
       label="Get new posts by email:"
       name="email"
     >
-      <UInput v-model="state.email" />
+      <UInput
+        v-model="state.email"
+        name="email"
+      />
+      <input
+        type="hidden"
+        name="email"
+        :value="state.email"
+      />
     </UFormField>
 
     <UButton
@@ -23,6 +32,7 @@
 </template>
 
 <script lang="ts" setup>
+  import type { FormSubmitEvent } from '@nuxt/ui'
   import * as z from 'zod'
 
   const schema = z.object({
@@ -34,4 +44,11 @@
   const state = reactive<Partial<Schema>>({
     email: undefined,
   })
+
+  async function onSubmit(event: FormSubmitEvent<Schema>) {
+    const formElement = event.target as HTMLFormElement
+    if (formElement) {
+      formElement.submit()
+    }
+  }
 </script>
